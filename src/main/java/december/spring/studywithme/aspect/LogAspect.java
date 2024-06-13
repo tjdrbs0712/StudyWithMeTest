dd8efc71ecbcee37d41f26cf8e6ca9cd69ff461b
tjdrbs0712 <102494118+tjdrbs0712@users.noreply.github.com>
2024-06-13 16:37:46 +0900

    AOP 추가하기

--- src/main/java/december/spring/studywithme/aspect/LogAspect.java
+++ src/main/java/december/spring/studywithme/aspect/LogAspect.java
@@ -1,20 +1,43 @@
 package december.spring.studywithme.aspect;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Objects;
 
 @Slf4j
 @Aspect
 @Component
 public class LogAspect {
     // 포인트컷 시그니처
+    @Pointcut("execution(* december.spring.studywithme.controller..*(..))")
+    public void controller(){}
+
     @Pointcut("execution(* december.spring.studywithme..*Service.*(..))")
     public void service() {}
 
+    //controller
+    @Before("controller()")
+    public void controllerLogBefore(JoinPoint joinPoint) throws Throwable{
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            // URL과 HTTP 메서드를 로그에 출력
+            log.info("Request URL: {}, HTTP METHOD: {}", request.getRequestURL(), request.getMethod());
+        } else {
+            log.warn("HttpServletRequest를 사용할 수 없습니다.");
+        }
+    }
+
+
+    // service
     @Before("service()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
         log.info("[Entering] {} with arguments: {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
--- src/main/java/december/spring/studywithme/exception/GlobalExceptionHandler.java
+++ src/main/java/december/spring/studywithme/exception/GlobalExceptionHandler.java
@@ -1,44 +1,42 @@
 package december.spring.studywithme.exception;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import december.spring.studywithme.dto.ErrorMessage;
 import december.spring.studywithme.dto.ResponseMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		
-		Map<String, String> errors = e.getBindingResult().getFieldErrors().stream()
-			.collect(Collectors.toMap(
-				error -> error.getField(),
-				error -> error.getDefaultMessage(),
-				(existingValue, newValue) -> existingValue
-			));
-		
-		ObjectMapper mapper = new ObjectMapper();
-		String response = "";
-		try {
-			response = mapper.writeValueAsString(errors);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		Map<String, String> errors = new HashMap<>();
+		e.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
 
 		ErrorMessage errorMessage = ErrorMessage.builder()
 				.statusCode(HttpStatus.BAD_REQUEST.value())
-				.message(response)
+				.message(errors.toString())
 				.build();
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
 	}
 	
 	@ExceptionHandler({UserException.class, PostException.class, EmailException.class,
